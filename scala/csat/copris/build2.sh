6bbe116bb34d707ad7d55c354b387bf8768b6298
Jaety Edwards <jaetye@gmail.com>
2013-06-02 14:52:59 -0400

    small changes to copris install scripts

--- scala/csat/copris/build2.sh
+++ scala/csat/copris/build2.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd copris-v2-0-0/examples
-scalac -cp ../lib/copris-all-v2-0-0.jar -d classes Examples.scala
-scala -cp classes:../lib/copris-all-v2-0-0.jar FirstStep
+./coprisc Examples.scala
+./copris FirstStep
--- scala/csat/copris/readme.txt
+++ scala/csat/copris/readme.txt
@@ -1,1 +1,8 @@
 source: http://bach.istc.kobe-u.ac.jp/copris/
+
+todo: fix warnings. Set up with Copris
+
+1. download
+2. use the sugar and sat4j libs right from the lib directory.  
+3. compile copris, create a jar
+4. 
